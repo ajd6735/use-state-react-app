ab380d67deb0cea5b0398d45bd0e5df4e6d565c4
ajd6735 <117790810+ajd6735@users.noreply.github.com>
2024-03-03 00:55:45 -0500

    creates a desktop app to learn react states

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+import messages from './messages'
+import { useState } from 'react';
+
+function App() {
+  const btnStyle = {
+    backgroundColor: "#7950f2",
+    color: "#fff"
+  }
+  const [step, setStep] = useState(1);
+  // const [isOpen, setIsOpen] useState(true);
+
+  return (
+    <div className='steps'>
+      <div className='numbers'>
+        <div className={step >= 1 ? `active` : ""}>1</div>
+        <div className={step >= 2 ? `active` : ""}>2</div>
+        <div className={step >= 3 ? `active` : ""}>3</div>
+      </div>
+      <p className='message'>
+        Step {step}: {messages[step -1]}
+      </p>
+      <div className='buttons'>
+        <button style={btnStyle} onClick={() => {if(step > 1) setStep(step -1)}}>Previous</button>
+        <button style={btnStyle} onClick={() => {if(step < 3) setStep(step + 1)}}>Next</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
